b59ecde568fbb7f390bc96ae2dca598a97421f29
Paolo Cosentino <cosepaol88@gmail.com>
2015-01-19 21:43:03 -0600

    added functionality

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
 	$(".btn-success").on('click', function(){
 		$(".correct").addClass("show");
 		$(".explanation").addClass("show");	
+		$(".incorrect").removeClass("show")
+	});
+
+	$(".btn-danger").on('click', function(){
+		$(".correct").removeClass("show")
+		$(".incorrect").addClass("show");
+		$(".explanation").addClass("show");	
 	});
 });
