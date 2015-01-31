113eaa4b0e7ad3755cc2fe19000ec167052b0eaa
Paolo Cosentino <cosepaol88@gmail.com>
2015-01-30 19:30:21 -0600

    add reviews in revers order

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,7 @@ class ReviewsController < ApplicationController
   # GET /reviews
   # GET /reviews.json
   def index
-    @reviews = Review.all
+    @reviews = Review.all.reverse
   end
 
   # GET /reviews/1
