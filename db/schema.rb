b08a54ccc1a2975c04aedb0f6a74fb9785202c30
Paolo Cosentino <cosepaol88@gmail.com>
2015-01-30 18:55:31 -0600

    Add invisible date field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128231647) do
+ActiveRecord::Schema.define(version: 20150131002448) do
 
   create_table "reviews", force: true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150128231647) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "rating"
+    t.string   "date"
   end
 
 end
